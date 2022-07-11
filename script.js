5936dde5aa3c09d60933a844b611669d891c21fd
Rohit rajpoot <rohit.rajpoot_cs20@gla.ac.in>
2022-07-11 22:17:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = document.getElementById("student-name");
+const rollNo = document.getElementById("univesity-rollno");
+const address = document.getElementById("address");
+const date = document.getElementById("date");
+
+window.onload = () => {
+    name.innerHTML = "Rohit Rajpoot";
+    rollNo.innerHTML = "";
+    address.innerHTML = "GLA University, Mathura";
+    date.innerHTML = new Date().toDateString();
+}
